bc0a65bc7fad86eb3baee1229574c8ec24e91971
FrazyCrog <nikitaalecseev2002@mail.ru>
2020-04-28 10:49:07 +0300

    Теперь рыбы спавнятся рандомно от площади двух точек

--- Assets/Scripts/FisgManager.cs
+++ Assets/Scripts/FisgManager.cs
@@ -12,7 +12,8 @@ public class FisgManager : MonoBehaviour
     public Text name;
     public Text price_text;
     public UIObject3D UIModelView;
-    private GameObject SetSpawnFish; // точка спавна рыбы
+    private GameObject SetSpawnFish1; // 1 точка спавна рыбы
+    private GameObject SetSpawnFish2; // 2 точка спавна рыбы
     int tankSize = globalFlock.tankSize;
     private int player_money;
     private int price;
@@ -32,9 +33,9 @@ public class FisgManager : MonoBehaviour
 
             System.Array.Resize(ref GlobalVar.Fish, GlobalVar.Fish.Length + 1);
             fish.prefab.GetComponent<FishBehavior>().name = fish.name;
-            GlobalVar.Fish[GlobalVar.sv.CountFish] = Instantiate(fish.prefab, SetSpawnFish.transform.position, Quaternion.identity);
+            Vector3 randSpawn = new Vector3(Random.Range(SetSpawnFish1.transform.position.x,SetSpawnFish2.transform.position.x), SetSpawnFish1.transform.position.y, Random.Range(SetSpawnFish1.transform.position.z,SetSpawnFish2.transform.position.z));
+            GlobalVar.Fish[GlobalVar.sv.CountFish] = Instantiate(fish.prefab, randSpawn, Quaternion.identity);
             GlobalVar.Fish[GlobalVar.sv.CountFish].GetComponent<FishBehavior>().StartFish = true;
-            Debug.Log("StartFish = " + GlobalVar.Fish[GlobalVar.sv.CountFish].GetComponent<FishBehavior>().StartFish);
             GlobalVar.Fish[GlobalVar.sv.CountFish].GetComponent<flock>().enabled = false;
             GlobalVar.Fish[GlobalVar.sv.CountFish].transform.Rotate(90,0,0);
             GlobalVar.sv.CountFish++;
@@ -65,7 +66,8 @@ public class FisgManager : MonoBehaviour
     }
     void Start()
     {
-      SetSpawnFish = GameObject.Find("SetSpawnFish");
+      SetSpawnFish1 = GameObject.Find("SetSpawnFish1");
+      SetSpawnFish2 = GameObject.Find("SetSpawnFish2");
       if(fish.prefabUI){
         UIModelView._ObjectPrefab = fish.prefabUI.transform;
       }
